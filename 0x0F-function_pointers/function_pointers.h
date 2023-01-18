7dce393ec8a5d75eb37d788be97ffeff08a6bd19
LertonJosine <josinelerton@gmail.com>
2023-01-18 11:25:59 -0800

    task 2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,5 @@
 #define HEADER_FILE function_pointers.h
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
-
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
