79740a6f1c5098f16bf58dff38e3237e1d5c19e1
LertonJosine <josinelerton@gmail.com>
2023-01-18 11:05:31 -0800

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE function_pointers.h
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 
 #endif
