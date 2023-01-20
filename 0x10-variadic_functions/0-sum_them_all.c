87d7a7fc1f0448f8a2644ed5b68fa97409c3daa6
LertonJosine <josinelerton@gmail.com>
2023-01-19 21:39:30 -0800

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...) {
+    if (n == 0) return 0;
+
+    va_list args;
+    va_start(args, n);
+
+    int sum = 0;
+    for (unsigned int i = 0; i < n; i++) {
+        sum += va_arg(args, int);
+    }
+
+    va_end(args);
+
+    return sum;
+}
